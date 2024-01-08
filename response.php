ebe937c21fccb1cc013f1f75eed90744ec621efa
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-08 21:51:05 +0100

    fix: testo cenurato

--- response.php
+++ response.php
@@ -1,6 +1,7 @@
 <?php
 $get_text = $_GET['text'];
 $get_badword = $_GET['badword'];
+$censored_text = str_replace($get_badword, '***', $get_text);
 ?>
 
 <!DOCTYPE html>
@@ -10,25 +11,28 @@ $get_badword = $_GET['badword'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Risposta</h1>
-    <p>
-        <?php
-        echo $get_text;
-        ?>
-    </p>
-    <div>
-        Il paragrafo è lungo <?php echo strlen($get_text); ?> caratteri.
-    </div>
-    <p>
-        <?php
-        echo str_replace($get_badword, '***', $get_text);
-        ?>
-    </p>
-    <div>
-        Il paragrafo è lungo <?php echo strlen($get_text); ?> caratteri.
+    <div class="container">
+        <h1>Risposta</h1>
+        <p>
+            <?php
+            echo $get_text;
+            ?>
+        </p>
+        <div>
+            Il paragrafo è lungo <?php echo strlen($get_text); ?> caratteri.
+        </div>
+        <p>
+            <?php
+            echo $censored_text;
+            ?>
+        </p>
+        <div>
+            Il paragrafo è lungo <?php echo strlen($censored_text); ?> caratteri.
+        </div>
     </div>
 </body>
 
